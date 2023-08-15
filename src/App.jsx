f9b675ea0f7b6dbe880fe834e9ae8e5bc4302bb5
unknown <ggug9289@gmail.com>
2023-08-15 16:45:22 -0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import FeedForm from "./component/FeedForm";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          margin: "0 auto",
+          width: "38rem",
+          minHeight: "auto",
+          background: "#3AB8E8",
+          borderRadius: "0.5rem",
+        }}>
+        <FeedForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
